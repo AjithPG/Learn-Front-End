54ca80c2a5de5182ac361467704ec9691832bade
AjithPG <ajithpg008@gmail.com>
2020-03-26 10:40:42 +0530

    added proper comments and remove unused codes and styles

--- Base Apparel coming soon page/script.js
+++ Base Apparel coming soon page/script.js
@@ -1,24 +1,23 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
-const alert = document.getElementById('alert');
+
 form.addEventListener('submit',e =>{
-e.preventDefault();
+
+e.preventDefault();//to prevent page reload while click submit button
+
 const emailVal = email.value;
 //check email validation
 if(!validateEmail(emailVal)){
   form.classList.add('error');
   form.classList.remove('success');
-  
-  
 }
 else{
     form.classList.remove('error');
     form.classList.add('success');
 }
-
-
 });
 
+//email validation regExp
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
